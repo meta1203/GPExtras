03211782d00948863f395ca316a3f949f6bf3c2d
Hunter Hancock <hancock.hunter@gmail.com>
2013-06-27 09:07:01 -0600

    First commit

---
+++ src/main/java/me/meta1203/plugins/gpextras/Gpextras.java
@@ -0,0 +1,22 @@
+package me.meta1203.plugins.gpextras;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Gpextras extends JavaPlugin implements Listener {
+    public void onDisable() {
+        // TODO: Place any custom disable code here.
+    }
+
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        event.getPlayer().sendMessage("Welcome, " + event.getPlayer().getDisplayName() + "!");
+    }
+}
+
